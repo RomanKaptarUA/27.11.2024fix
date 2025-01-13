4ded5b4b0a62d4a5f1ac70a4f23f340b3fca6d61
RomanKaptarUA <romankaptar82@gmail.com>
2025-01-13 13:19:25 +0200

    б

--- src/index.js
+++ src/index.js
@@ -145,61 +145,90 @@
 //     console.log('Finish')
 // }, seconds * 1000);
 
-const display = document.getElementById("stopwatch-display");
-const startBtn = document.getElementById("start-stopwatch");
-const pauseBtn = document.getElementById("pause-stopwatch");
-const resetBtn = document.getElementById("reset-stopwatch");
 
-let startTime = 0;
-let elapsedTime = 0;
-let timerInterval;
-let lastUpdate = 0;
 
-function formatTime(ms) {
-  const hours = Math.floor(ms / (1000 * 60 * 60));
-  const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((ms % (1000 * 60 * 60)) / 1000);
-  const milliseconds = Math.floor(ms % 1000);
 
-  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
-    2,
-    "0"
-  )}:${String(seconds).padStart(2, "0")}:${String(milliseconds).padStart(
-    3,
-    "0"
-  )}`;
-}
 
-function updateDisplay() {
-  const currentTime = performance.now();
-  elapsedTime = currentTime - lastUpdate + elapsedTime;
-  lastUpdate = currentTime;
 
-  display.textContent = formatTime(Math.round(elapsedTime));
-}
+// Таймер
+// const display = document.getElementById("stopwatch-display");
+// const startBtn = document.getElementById("start-stopwatch");
+// const pauseBtn = document.getElementById("pause-stopwatch");
+// const resetBtn = document.getElementById("reset-stopwatch");
 
-startBtn.addEventListener("click", () => {
-  startBtn.classList.add("hidden");
-  pauseBtn.classList.remove("hidden");
-  resetBtn.classList.remove("hidden");
+// let startTime = 0;
+// let elapsedTime = 0;
+// let timerInterval;
+// let lastUpdate = 0;
 
-  lastUpdate = performance.now();
-  timerInterval = setInterval(updateDisplay, 10);
-});
+// function formatTime(ms) {
+//   const hours = Math.floor(ms / (1000 * 60 * 60));
+//   const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((ms % (1000 * 60 * 60)) / 1000);
+//   const milliseconds = Math.floor(ms % 1000);
 
-pauseBtn.addEventListener("click", () => {
-  pauseBtn.classList.add("hidden");
-  startBtn.classList.remove("hidden");
+//   return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+//     2,
+//     "0"
+//   )}:${String(seconds).padStart(2, "0")}:${String(milliseconds).padStart(
+//     3,
+//     "0"
+//   )}`;
+// }
 
-  clearInterval(timerInterval);
-});
+// function updateDisplay() {
+//   const currentTime = performance.now();
+//   elapsedTime = currentTime - lastUpdate + elapsedTime;
+//   lastUpdate = currentTime;
 
-resetBtn.addEventListener("click", () => {
-  clearInterval(timerInterval);
-  elapsedTime = 0;
-  display.textContent = "00:00:00.000";
+//   display.textContent = formatTime(Math.round(elapsedTime));
+// }
 
-  startBtn.classList.remove("hidden");
-  pauseBtn.classList.add("hidden");
-  resetBtn.classList.add("hidden");
-});
+// startBtn.addEventListener("click", () => {
+//   startBtn.classList.add("hidden");
+//   pauseBtn.classList.remove("hidden");
+//   resetBtn.classList.remove("hidden");
+
+//   lastUpdate = performance.now();
+//   timerInterval = setInterval(updateDisplay, 10);
+// });
+
+// pauseBtn.addEventListener("click", () => {
+//   pauseBtn.classList.add("hidden");
+//   startBtn.classList.remove("hidden");
+
+//   clearInterval(timerInterval);
+// });
+
+// resetBtn.addEventListener("click", () => {
+//   clearInterval(timerInterval);
+//   elapsedTime = 0;
+//   display.textContent = "00:00:00.000";
+
+//   startBtn.classList.remove("hidden");
+//   pauseBtn.classList.add("hidden");
+//   resetBtn.classList.add("hidden");
+// });
+
+
+
+// Таймер 1
+// let timer = 10000;
+// const timerEl = document.getElementById('timer');
+
+// const timerInterval = setInterval(() => {
+//   timer -= 1000;
+//   timerEl.textContent = (timer / 1000).toFixed(0);
+
+//   if(timer <= 5000) {
+//     document.body.style.backgroundColor = 'yellow';
+//   }
+
+//   if(timer <=0) {
+//     clearInterval(timerInterval)
+//     timerEl.textContent = '0';
+//   }
+// }, 1000);
+
+
+// Таймер 2
