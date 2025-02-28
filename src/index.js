451c8f88d1517c8c0d9bce2a709b27a4484e4f27
RomanKaptarUA <romankaptar82@gmail.com>
2025-02-28 19:06:30 +0200

    ю

--- src/index.js
+++ src/index.js
@@ -1411,16 +1411,56 @@
 
 ////////////////////////////////
 
-const headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json",
-  "X-Custom-Header": "custom value",
-  "Authorization": "YOUR_API_TOKEN"
-}
+// const headers = {
+//   "Accept": "application/json",
+//   "Content-Type": "application/json",
+//   "X-Custom-Header": "custom value",
+//   "Authorization": "YOUR_API_TOKEN"
+// }
 
-fetch("https://api.example.com/data", {
-  method: "GET",
-  headers: headers,
-}).then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error("Error:", error));
+// fetch("https://api.example.com/data", {
+//   method: "GET",
+//   headers: headers,
+// }).then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error("Error:", error));
+
+//////////////////////////////////////////////
+// покемони
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.js-search-form');
+  const cardContainer = document.querySelector('.js-card-container');
+
+  if (!form || !cardContainer){
+    console.error('Форма або контейнер не знайдені');
+    return;
+  }
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const query = form.query.value.trim().toLowerCase();
+    if (!query) return;
+
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+      if (response.ok) {
+        const data = await response.json();
+        render(data);
+      } else {
+        cardContainer.innerHTML = `<p class="error">Покемон не знайдений</p>`
+      }
+      form.reset();
+  });
+
+  function render(pokemon) {
+          cardContainer.innerHTML = `
+       <div class="card">
+        <h2>${pokemon.name}</h2>
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <p><strong>Вага: </strong>${pokemon.weight}</p>
+        <p><strong>Зріст: </strong>${pokemon.height}</p>
+       </div>
+       `;
+  }
+
+})
